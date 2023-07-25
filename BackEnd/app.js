fa3362b70864cebe725ceabb4422f5745d8a1e03
mrguven <mrguven@gmail.com>
2023-07-25 23:39:50 +0200

    update

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -4,6 +4,20 @@ const mySql= require('mysql');
 
 const app= express();
 
+const dotenv=require('dotenv').config();
+
+const db=mySql.createConnection({
+    host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+
+
+db.connect();
+
+app.use(express.urlencoded({extended:true}));
+
 
 app.listen(4000, ()=>{
     console.log('connected the server');
