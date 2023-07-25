d33040f5f533e450918bd2fa295c99bf2b2a5c4c
mrguven <mrguven@gmail.com>
2023-07-25 23:56:34 +0200

    add button

--- src/pages/CatchMe.jsx
+++ src/pages/CatchMe.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
+
+
 export default function CatchMe(){
+
+const [heart,setHeart]=useState();
+const [location,setLocation]=useState();
+const [start,setStart]=useState();
+const[con,setCon]=useState();
+
+
+
 
 
 
 
     return(
 
+<div>
+<div><h1 id="title">Catch Me</h1></div>
 
-<h1 id="title">Catch Me</h1>
+<button id="startButton">Start</button>
+
+<div id="border"> </div>
+</div>
+
+
+
+
+
+
 
     )
 }
