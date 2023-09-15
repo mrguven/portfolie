d28febfa997ec4365c976c0d99f1202e3902f41a
mrguven <mrguven@gmail.com>
2023-09-15 13:08:20 +0200

    update

--- src/pages/MyTaxi.jsx
+++ src/pages/MyTaxi.jsx
@@ -8,9 +8,47 @@ const head= {
     
 }
 
-    return(
 
+const departureStyle={
+    fontSize:'30px',
+    margin:'15px'
+}
+const arrivingStyle={
+    fontSize:'30px',
+    margin:'15px'
+}
+
+    return(
+<div>
         <h1 style={head}>MyTaxi</h1>
+
+
+
+<form action="" method="get">
+    <div>
+         <div>
+                  <label htmlFor="departure" style={departureStyle}  >
+                    Departure:   <input type="text" name="departure" id="departure" />
+                  </label>
+        </div>
+        <div>
+                  <label htmlFor="arriving" style={arrivingStyle}>
+                    Arriving: <input type="text" name="arriving" id="arriving" />
+                 </label>
+        </div>
+        <div>
+            <label htmlFor="date"> Date 
+             <input type="datetime-local" name="time" id="time" />
+            
+            </label>
+        </div>
+    </div>
+</form>
+
+
+
+
+</div>
 
     )
 }
