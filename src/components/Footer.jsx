7fb1be05d58c161e74d83b6153ab129c507bd9db
mrguven <mrguven@gmail.com>
2023-10-02 22:05:35 +0200

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,19 +9,19 @@ export default function Footer ( ) {
     return(
 <div id="footer">
 <div>
-<h2> <a href="https://www.linkedin.com/in/rauf-guven-a76791a6/">LinkedIn </a>  </h2>
+<h2> <a className="footerLinks" target="_blank" href="https://www.linkedin.com/in/rauf-guven-a76791a6/">LinkedIn </a>  </h2>
 
 </div>
 <div>
-<h2> <a href="https://github.com/mrguven">GitHub </a>  </h2>
+<h2> <a className="footerLinks" target="_blank" href="https://github.com/mrguven">GitHub </a>  </h2>
 
 </div>
 <div>
-<h2> <a href="https://www.linkedin.com/in/rauf-guven-a76791a6/">LinkedIn </a>  </h2>
+<h2> <a className="footerLinks" target="_blank" href="https://www.linkedin.com/in/rauf-guven-a76791a6/">LinkedIn </a>  </h2>
 
 </div>
 <div>
-<h2> <a href="https://www.linkedin.com/in/rauf-guven-a76791a6/">LinkedIn </a>  </h2>
+<h2> <a className="footerLinks" target="_blank" href="https://www.linkedin.com/in/rauf-guven-a76791a6/">LinkedIn </a>  </h2>
 
 </div>
 
