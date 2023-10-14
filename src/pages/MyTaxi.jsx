720b2c97dff067d99796095dfe58f722929e9c35
mrguven <mrguven@gmail.com>
2023-10-14 23:00:28 +0200

    update

--- src/pages/MyTaxi.jsx
+++ src/pages/MyTaxi.jsx
@@ -1,7 +1,7 @@
 
-import { useState,useEffect  } from 'react';
+import { useState,useEffect, useRef } from 'react';
 import { Loader } from '@googlemaps/js-api-loader';
- import GMap from '../pages/Gmap'
+// import GMap from '../pages/Gmap'
 // import axios from 'axios';
 import { useJsApiLoader } from "@react-google-maps/api";
 //import { usePlacesWidget,Autocomplete } from "react-google-autocomplete";
@@ -21,7 +21,18 @@ import { useJsApiLoader } from "@react-google-maps/api";
     //     RequestType,
     //   } from "react-geocode";
       
-    
+    import {
+      setKey,
+      setDefaults,
+      setLanguage,
+      setRegion,
+      fromAddress,
+      fromLatLng,
+      fromPlaceId,
+      setLocationType,
+      geocode,
+      RequestType,
+    } from "react-geocode";
 
     
 
@@ -44,19 +55,34 @@ const[travelTime,setTravelTime]=useState();
 const [directionsResponse, setDirectionsResponse] = useState(null);
 const [loadMap, setLoadMap] = useState(false);
 
-useEffect(() => {
-  const options = {
-    apiKey: GOOGLE_MAP_API_KEY,
-    version: "weekly",
-    libraries: ['geometry']
-  };
 
-  new Loader(options).load().then(() => {
-    setLoadMap(true);
-  }).catch(e => {
-    console.error('Sorry, something went wrong: Please try again later. Error:', e);
-  });
+setDefaults({
+  key:  GOOGLE_MAP_API_KEY 
+  
+});
+
+const googleMapRef = useRef(null);
+const [map, setMap] = useState(null);
+
+useEffect(() => {
+  const googleMap = initGoogleMap();
+  setMap(googleMap);
 }, []);
+
+
+// useEffect(() => {
+//   const options = {
+//     apiKey: GOOGLE_MAP_API_KEY,
+//     version: "weekly",
+//     libraries: ['geometry']
+//   };
+
+//   new Loader(options).load().then(() => {
+//     setLoadMap(true);
+//   }).catch(e => {
+//     console.error('Sorry, something went wrong: Please try again later. Error:', e);
+//   });
+// }, []);
 
 
 
@@ -64,10 +90,10 @@ console.log(time);
 console.log(arriving);
 console.log(departure);
 
-const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: process.env.google_maps_api_key,
-  });
+// const { isLoaded } = useJsApiLoader({
+//     id: "google-map-script",
+//     googleMapsApiKey: process.env.google_maps_api_key,
+//   });
 
 
 
@@ -76,28 +102,70 @@ const makeReservation = async (event)=>{
        
 
         if (arriving === "" || departure === "") {
-            return;
-          } else  {
-            const directionsService = new window.google.maps.DirectionsService();
-            const results = await directionsService.route({
-              origin: departure,
-              destination: arriving,
-              travelMode: window.google.maps.TravelMode.DRIVING,
+          const initGoogleMap = () => {
+            return new window.google.maps.Map(googleMapRef.current, {
+              center: new window.google.maps.LatLng(51.9244, 4.4777),
+              zoom: 10
             });
-            setDirectionsResponse(results);
-            console.log(directionsResponse);
-  
           }
+    
+          initGoogleMap();
+          } 
        
          
 
+          if (!map) return;
 
+          var directionsService = new window.google.maps.DirectionsService();
+          var directionsRenderer = new window.google.maps.DirectionsRenderer();
+      if(arriving  && departure) {fromAddress( departure)
+        .then(({ results }) => {
+          const { lat, lng } = results[0].geometry.location;
+          console.log(lat, lng);
+          var departure = new window.google.maps.LatLng(lat, lng);
+       
+        
+        
+        fromAddress(arriving)
+        .then(({ results }) => {
+          const { lat, lng } = results[0].geometry.location;
+          console.log(results);
+          var arriving = new window.google.maps.LatLng(lat, lng);
+          var request = {
+            origin: departure,
+            destination: arriving,
+            travelMode: 'DRIVING'
+          };
+          directionsService.route(request, function (response, status) {
+            if (status == 'OK') {
+              directionsRenderer.setDirections(response);
+              console.log(response);
+              directionsRenderer.setMap(map);
+              console.log(map);
+            }
+          });
+      
+      
+        })
+        .catch(console.error);
+      
+        
+      })
+      .catch(console.error);}
+      else {
+        return
+      }
 
-
+   
 
         }
 
-
+        const initGoogleMap = () => {
+          return new window.google.maps.Map(googleMapRef.current, {
+            center: new window.google.maps.LatLng(51.9244, 4.4777),
+            zoom: 10
+          });
+        }
 
         // const { ref } = usePlacesWidget({
         //   apiKey: GOOGLE_MAP_API_KEY,
@@ -203,12 +271,18 @@ const makeReservation = async (event)=>{
       </GoogleMap>
 } */}
 
-    <div className="App">
+    {/* <div className="App">
      
       { !directionsResponse ? <div>Loading...</div> : <GMap   arrPlace={arriving} deparPlace={departure}   />}
      
      
-    </div>
+    </div> */}
+
+    <div id='mapsStyle'
+    ref={googleMapRef}
+    
+   
+  />
 
     <div>
     <h2 id='result'></h2>
