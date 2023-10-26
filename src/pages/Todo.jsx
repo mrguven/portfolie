bfef5dd5fd79711c9a7b29aeea18b262fab4d9f8
mrguven <mrguven@gmail.com>
2023-10-26 23:16:37 +0200

    update

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,12 +1,43 @@
+import { useRef, useState } from "react"
+
 export default function Todo(){
+
+const [todoList,setTodoList]=useState([]);
+const [todo,setTodo]=useState('');
+const result=useRef();
+
+
+const addToList=()=>{
+console.log('hello');
+setTodoList(
+    [...todoList,todo]
+)
+
+console.log(todoList);
+
+}
+
 
 
 
 return(
 
+<div>
+
+<h1>Toto List</h1>
 
 
-    <h1>Toto List</h1>
+<input type="text" name="" id="todo"  placeholder="write your tasks"
+  onChange={(e)=>{setTodo(e.target.value)}} value={todo}/>
+<button  onClick={addToList} >add</button>
+
+<h3 ref={result} ></h3>
+
+</div>
+
+ 
+
+
 )
 
 
