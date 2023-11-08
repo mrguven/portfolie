7d71ea6a7dd9c537850dc2b41c97456653d3a7cc
mrguven <mrguven@gmail.com>
2023-11-08 23:42:33 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,39 @@
 export default function Home(){
 
 
+
+
+
+
+
+
     return(
 
 
-        <h1>MY JOURNEY</h1>
+<div>
+
+<h1>Ontdek Ons</h1>
+<h2>Je kan hier vinden over mijn werken. </h2>
+<h3>Portfolie</h3>
+<h3>Taxi Project...</h3>
+<h3>Todo App..</h3>
+<h3>CatchMe Games..</h3>
+
+<div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+
+</div>
+
+
+
+
+
+       
+       
+
+        </div>
     )
 }
