1fe1ea5e4023c559264e997ab3e53d953d92eb18
mrguven <mrguven@gmail.com>
2023-11-09 00:55:35 +0100

    update homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,18 +12,20 @@ export default function Home(){
 
 <div>
 
+<div>
 <h1>Ontdek Ons</h1>
 <h2>Je kan hier vinden over mijn werken. </h2>
 <h3>Portfolie</h3>
 <h3>Taxi Project...</h3>
 <h3>Todo App..</h3>
 <h3>CatchMe Games..</h3>
+</div>
 
-<div>
-<div></div>
-<div></div>
-<div></div>
-<div></div>
+<div className="Container">
+<div className="subCont"></div>
+<div className="subCont"></div>
+<div className="subCont"></div>
+<div className="subCont"></div>
 
 </div>
 
