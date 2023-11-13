b2dc15a8d390c9b32f01c77e6a67848c04527d29
mrguven <mrguven@gmail.com>
2023-11-13 23:13:11 +0100

    update homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,19 +16,32 @@ export default function Home(){
 <div>
 
 <div id='infoContainer'>
-<div>
-<img src={mrg} alt="profil foto"  id='profil'/>
+<div id='profil'>
+<img src={mrg} alt="profil foto"  id='foto'/>
+<h2>Wie ben Ik?</h2>
 </div>
 
-<div id='subInfo'>
+<div className='classSubinfo'  id='subInfo'>
 <h1>Communicatief, sociaal, gedreven, geordend, analytisch,
      oplossingsgericht, professioneel en resultaatgericht 
      zijn kwaliteiten die mij omschrijven. Ik ben een toegewijde
       medewerker met ervaring in het helpen van organisaties
-       om bedrijfsdoelen te behalen en te overtreffen. Ik heb 
+       om bedrijfsdoelen te behalen en te overtreffen.
+       </h1>
+</div>
+<div className='classSubinfo'  id='subInfo-2'>
+    <h1> Ik heb 
        een passie voor ICT en ik leer snel. Ik heb een oog voor
         detail waarbij ik ICT-knelpunten tijdig identificeer.
-         Dit doe ik op een vrolijke en gestructureerde manier zonder aandacht te verliezen voor de organisatiedoelstellingen. </h1>
+         Dit doe ik op een vrolijke en gestructureerde manier zonder aandacht te verliezen voor de organisatiedoelstellingen.
+</h1>
+</div>
+<div className='classSubinfo'  id='subInfo-3'>
+    <h1>Ik heb een oog voor
+        detail waarbij ik ICT-knelpunten tijdig identificeer.
+         Dit doe ik op een vrolijke en gestructureerde manier
+          zonder aandacht te verliezen voor de organisatiedoelstellingen.
+</h1>
 </div>
 <div id='port'>
 <h3>Portfolie</h3>
