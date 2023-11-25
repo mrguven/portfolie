877464253879098ae556889432816260b19d88c5
mrguven <mrguven@gmail.com>
2023-11-25 17:22:07 +0100

    update playlist page

--- src/pages/MyPlayList.jsx
+++ src/pages/MyPlayList.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import nextButton from '../nextButton.png'
+
 
 export default function MyPlayList () {
 
@@ -12,11 +14,21 @@ export default function MyPlayList () {
 <div id="playListContainer">
 <h1> MyPlay List</h1>
 
-<div id="playlistInput"> <input type="text" name="music" id="musicInput" /></div>
-<div></div>
-<div></div>
-<div></div>
-<div></div>
+<div id="playlistInput"> 
+ <input type="text" name="music" id="musicInput" placeholder="search..."/>
+ </div>
+<div id="search-Music-Div">
+<button type="submit" id="search-Music-Button"> +</button>
+</div>
+<div id="music-Suggestions">
+    <ul>
+        <li></li>
+    </ul>
+</div>
+<div id="playlist-Image"></div>
+<div id="control-Panel">
+    <img className="control-Panel-Image" src={nextButton} alt="" />
+</div>
 
 
 </div>
