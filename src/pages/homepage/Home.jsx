b3cc48b7568c19db8ba11a54a847cd4ec76fcd8f
mrguven <mrguven@gmail.com>
2024-02-15 21:27:13 +0100

    update

--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -8,23 +8,42 @@ export default function Home(){
 
 
 
+window.onload=evet=>{
 
+  let profil= document.getElementById('profilePicture')
+profil.style.transform= 'scale(1.5)';
+profil.style.transition='3s'
 
+}
+
+function getSmall(){
+  let profil= document.getElementById('profilePicture');
+profil.style.transform= 'scale(1)';
+profil.style.transition='3s'
+}
+
+function getValueInPut(){
+
+}
 
 
     return(
 <div id='mainContainerHome' >
 
   <div id='profileDiv'>  
-    
+   <p id='text1' >sdfssfdsfdsdfsdfsd</p>
+
+
     
   </div>
-  <div></div>sdfsdfsdfsd
-  <div></div>
+  <div><img src={mrg} alt="profil" id='profilePicture' onClick={getSmall} /></div>
+  <div> <input type="text" name="name" id="test1" onClick={getValueInPut} />  </div>
   <div></div>
   <div></div>
   <div></div>
    
+
+
 </div>
 
     )
